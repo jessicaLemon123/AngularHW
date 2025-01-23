eea86639ae1148cbc81dc1ea456ecfd3ca50ff36
jessicaccLemon123 <jessicacclemon@gmail.com>
2025-01-23 14:02:16 +0800

    practice

--- 01-starting-project 2/src/app/dashboard/server/server.component.ts
+++ 01-starting-project 2/src/app/dashboard/server/server.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  DestroyRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -7,13 +13,13 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
   styleUrl: './server.component.css',
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
-
+  private destroyRef = inject(DestroyRef);
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -24,5 +30,9 @@ export class ServerComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 }
